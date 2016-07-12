f9222e71e39a5c86315dc990b3dc10d0c415bd57
root <root@localhost.localdomain>
2016-07-12 09:37:47 -0400

    75%

---
+++ format.js
@@ -0,0 +1,7 @@
+$('[type="submit"]').on('click', function () {
+    // this adds 'required' class to all the required inputs under the same <form> as the submit button
+    $(this)
+        .closest('form')
+        .find('[required]')
+        .addClass('required');
+});
